c2f8f92a1cdfda298f20b3b5ad807ca3ed217c30
Sonam Lama <sonamtlama@gmail.com>
2019-03-12 23:51:55 -0700

    adding all files

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+// Templated Friends Data
+var friendsData = [
+    {
+      name: "Sonam",
+      photo: "https://www.facebook.com/photo.php?fbid=10158623473755231&set=a.10150262255580231&type=3&theater",
+      scores: [3, 4, 3, 3, 5, 4, 4, 2, 5, 3]
+    },
+    {
+      name: "R2D2",
+      photo:"https://en.wikipedia.org/wiki/R2-D2#/media/File:R2-D2_Droid.png",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Wall-E",
+      photo:"https://www.imdb.com/title/tt0910970/mediaviewer/rm1659211008",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Rosie the Robot",
+      photo:"https://thejetsons.fandom.com/wiki/Rosey?file=Rosey.jpg",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Bender",
+      photo:"https://en.wikipedia.org/wiki/Bender_(Futurama)#/media/File:Bender_Rodriguez.png",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Geoff Petersen",
+      photo:"https://en.wikipedia.org/wiki/Geoff_Peterson#/media/File:Geoff_Peterson.png",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Vicki the robot",
+      photo:"https://www.yahoo.com/entertainment/small-wonder-30th-anniversary-vicki-1980s-tv-128290305450.html",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
