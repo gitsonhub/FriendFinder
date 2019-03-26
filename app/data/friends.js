619047f2c0fa64acd59eeb889b8c6c6b6c2cf213
Sonam Lama <sonamtlama@gmail.com>
2019-03-26 11:38:33 -0700

    updates on the files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,43 +1,41 @@
-// Templated Friends Data
+// List of possible friends
 var friendsData = [
     {
-      name: "Sonam",
-      photo: "https://www.facebook.com/photo.php?fbid=10158623473755231&set=a.10150262255580231&type=3&theater",
-      scores: [3, 4, 3, 3, 5, 4, 4, 2, 5, 3]
+      name: "RoboCop",
+      photo: "https://upload.wikimedia.org/wikipedia/en/1/16/RoboCop_%281987%29_theatrical_poster.jpg",
+      scores: [4, 3, 3, 4, 3, 4, 4, 3, 4, 3]
     },
     {
       name: "R2D2",
-      photo:"https://en.wikipedia.org/wiki/R2-D2#/media/File:R2-D2_Droid.png",
+      photo:"https://upload.wikimedia.org/wikipedia/en/3/39/R2-D2_Droid.png",
       scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
     },
     {
       name: "Wall-E",
-      photo:"https://www.imdb.com/title/tt0910970/mediaviewer/rm1659211008",
+      photo:"https://images-na.ssl-images-amazon.com/images/I/51iA8isg59L._SY445_.jpg",
       scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     },
     {
       name: "Rosie the Robot",
-      photo:"https://thejetsons.fandom.com/wiki/Rosey?file=Rosey.jpg",
+      photo:"https://vignette.wikia.nocookie.net/thejetsons/images/6/6f/Rosey.jpg/revision/latest?cb=20120109214359",
       scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     },
     {
       name: "Bender",
-      photo:"https://en.wikipedia.org/wiki/Bender_(Futurama)#/media/File:Bender_Rodriguez.png",
-      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+      photo:"https://upload.wikimedia.org/wikipedia/en/a/a6/Bender_Rodriguez.png",
+      scores: [4, 4, 4, 4, 4, 4, 4, 4, 5, 5]
     },
     {
       name: "Geoff Petersen",
-      photo:"https://en.wikipedia.org/wiki/Geoff_Peterson#/media/File:Geoff_Peterson.png",
-      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+      photo:"https://upload.wikimedia.org/wikipedia/en/e/e9/Geoff_Peterson.png",
+      scores: [2, 4, 3, 4, 2, 3, 3, 4, 4, 3]
     },
     {
-      name: "Vicki the robot",
-      photo:"https://www.yahoo.com/entertainment/small-wonder-30th-anniversary-vicki-1980s-tv-128290305450.html",
-      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+      name: "Vicki the Robot",
+      photo:"https://s.yimg.com/ny/api/res/1.2/0ZjdbjNv1oVtoRbM0ZkvWg--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9MTI4MDtoPTk2MDtpbD1wbGFuZQ--/http://40.media.tumblr.com/a4362a7d0ca3bb9f46fdc7ae1bb874e4/tumblr_inline_nvk0jrXSkC1spviij_540.jpg",
+      scores: [5, 4, 5, 4, 5, 5, 5, 5, 5, 5]
     }
   ];
-  
-  
-  
-  // Export to API Route
-  module.exports = friendsData;
+    
+// Export to API Route
+module.exports = friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,6 @@ var friendsData = require('../data/friends.js');
 
 // Includes Two Routes
 function apiRoutes(app) {
-
   // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
   app.get('/api/friends', function (req, res) {
     res.json(friendsData);
@@ -13,7 +12,6 @@ function apiRoutes(app) {
 
   // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
   app.post('/api/friends', function (req, res) {
-
     // Parse new friend input to get integers (AJAX post seemed to make the numbers strings)
     var newFriend = {
       name: req.body.name,
@@ -29,26 +27,22 @@ function apiRoutes(app) {
     // Cross check the new friend entry with the existing ones
     var scoreComparisionArray = [];
     for(var i=0; i < friendsData.length; i++){
-
       // Check each friend's scores and sum difference in points
       var currentComparison = 0;
       for(var j=0; j < newFriend.scores.length; j++){
         currentComparison += Math.abs( newFriend.scores[j] - friendsData[i].scores[j] );
       }
-
       // Push each comparison between friends to array
       scoreComparisionArray.push(currentComparison);
     }
 
     // Determine the best match using the postion of best match in the friendsData array
     var bestMatchPosition = 0; // assume its the first person to start
-    for(var i=1; i < scoreComparisionArray.length; i++){
-      
+    for(var i=1; i < scoreComparisionArray.length; i++){      
       // Lower number in comparison difference means better match
       if(scoreComparisionArray[i] <= scoreComparisionArray[bestMatchPosition]){
         bestMatchPosition = i;
       }
-
     }
 
     // ***NOTE*** If the 2 friends have the same comparison, then the NEWEST entry in the friendsData array is chosen
